dcdc636f5d0a54df26b13a56b514d6f253ea9f44
jinsetmnr <jinsetmnr@gmail.com>
2015-12-19 19:15:54 +0530

    Signed-off-by: jinsetmnr <jinsetmnr@gmail.com>

--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '../../dist');
+app.set('views', __dirname + '././dist');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
