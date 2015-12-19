7d636bcd72b7f44299d13f303a04646b6614fe4d
jinsetmnr <jinsetmnr@gmail.com>
2015-12-19 17:05:26 +0530

    Signed-off

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,3 @@
-/*eslint no-console: 0*/
-
-'use strict';
 
 var express = require('express');
 var path    = require('path');
@@ -20,9 +17,6 @@ app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname + '../../../dist/index.html'));
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
