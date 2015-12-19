e72284cf6d76f8b21a9007d279e81df84dc7ee03
jinsetmnr <jinsetmnr@gmail.com>
2015-12-19 19:53:04 +0530

    Signed-off-by: jinsetmnr <jinsetmnr@gmail.com>

--- src/client/main.js
+++
@@ -1,7 +0,0 @@
-/*eslint no-unused-vars: 0*/
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router';
-require ('./utils/GlobalWindowManager'); 
-
-ReactDOM.render((require('./router')), document.getElementById('app'));
