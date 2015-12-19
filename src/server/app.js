a1471ea02fb1b87c810ebd558e4cc37e3c8e28ee
jinsetmnr <jinsetmnr@gmail.com>
2015-12-19 19:04:29 +0530

    Signed-off-by: jinsetmnr <jinsetmnr@gmail.com>

--- src/server/app.js
+++ src/server/app.js
@@ -4,7 +4,6 @@ var path    = require('path');
 var app     = express();
 
 
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -13,13 +12,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-
-
 app.get('/', function(request, response) {
 	response.sendFile('dist/index.html' , { root : __dirname});
     
 });
-
 
 
 app.listen(app.get('port'), function() {
