4ffc57a65854e72455a09f2a466fa590a2aa10d8
jinsetmnr <jinsetmnr@gmail.com>
2015-12-20 14:46:19 +0530

    Signed-off-by: jinsetmnr <jinsetmnr@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var connect = require('gulp-connect');
 var gopen = require('gulp-open');
 var browserify = require('browserify');
 var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+var source = require('vinyl-source-stream'); 
 var concat = require('gulp-concat');
 var lint = require('gulp-eslint');
 var sass = require('gulp-sass');
@@ -28,13 +28,10 @@ var config = {
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
             'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
             'node_modules/react-bootstrap-switch/dist/css/bootstrap3/react-bootstrap-switch.min.css',
-            'node_modules/rc-datepicker/examples/example.css',
             '.tmp/**/*.css'
         ],
-        images: './src/client/images/**/*.*',
-        fonts:['node_modules/bootstrap/dist/fonts/**.*',
-               'assets/fonts/*.*'
-              ]
+        images: './src/client/images/**/*.svg',
+        fonts:'node_modules/bootstrap/dist/fonts/**.*'
     }
 };
 
@@ -71,7 +68,7 @@ gulp.task('bundlejs', function() {
 
 //compiling sass files to css
 gulp.task('sass', function (){
-
+      
     gulp.src(config.paths.sass)
     .on('error', console.error.bind(console))
 	.pipe(sass())
@@ -89,10 +86,13 @@ gulp.task('bundleimages', function () {
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
 
+    //publish favicon
+        //gulp.src('./src/favicon.ico')
+        //.pipe(gulp.dest(config.paths.dist));
 });
 
 gulp.task('bundlefonts', function() {
-    gulp.src(config.paths.fonts)
+    gulp.src(config.paths.fonts)	
 		.pipe(gulp.dest(config.paths.dist+'/fonts'));
 });
 
@@ -114,7 +114,7 @@ gulp.task('set-prod', function() {
     config.production = true;
 });
 
-gulp.task('assets', ['html',
+gulp.task('assets', ['html', 
                      'lint',
                      'bundlejs',
 					 'sass',
@@ -125,3 +125,5 @@ gulp.task('assets', ['html',
 gulp.task('build', ['set-prod', 'assets']);
 
 gulp.task('default', ['assets', 'open', 'watch']);
+
+ 
