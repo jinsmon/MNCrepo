3be0b7809944eff8e0b10ab848eb549019bff287
jinsetmnr <jinsetmnr@gmail.com>
2015-12-22 18:38:38 +0530

    Signed-off-by: jinsetmnr <jinsetmnr@gmail.com>

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var app     = express();
 var request=require('request');
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '../../../dist/'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '../../../dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 }
 );
 app.get('/api/worklists', function(req, res) {
