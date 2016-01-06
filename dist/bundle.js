58d31838e5dc5af5f5c65df0f837ad27339f52de
jinsetmnr <jinsetmnr@gmail.com>
2016-01-06 19:58:16 +0530

    commit at 7.22
    Signed-off-by: jinsetmnr <jinsetmnr@gmail.com>

--- dist/bundle.js
+++ dist/bundle.js
@@ -71198,8 +71198,8 @@ var PATIENT_VISIT_FETCH_URL = '/api/getVisits'; //'http://ec2-52-10-19-65.us-wes
 var PATIENT_DETAILS_URL = '/api/getPatientDetail'; //'http://ec2-52-10-19-65.us-west-2.compute.amazonaws.com/FHIRServer/patientRegistration/searchByID?id=';
 var FETCH_SUBCENTER_DETAILS = '/api/getSubCenterDetails';
 var PATIENT_MANUAL_SEARCH_URL = '/api/getWorkList';
-var PATIENT_SEARCH_URL = '/api/worklists'; //'http://ec2-52-10-19-65.us-west-2.compute.amazonaws.com/FHIRServer/patientRegistration/search';
-//const PATIENT_SEARCH_URL = 'http://ec2-52-34-194-19.us-west-2.compute.amazonaws.com/FHIRServer/patientRegistration/search';
+//const PATIENT_SEARCH_URL = '/api/worklists'; //'http://ec2-52-10-19-65.us-west-2.compute.amazonaws.com/FHIRServer/patientRegistration/search';
+var PATIENT_SEARCH_URL = 'http://ec2-52-34-194-19.us-west-2.compute.amazonaws.com/FHIRServer/patientRegistration/search';
 var REGISTRATION_URL_PATTERN = 'Developer/Registration/RegistrationId';
 var VISIT_SUMMARY_URL_PATTERN = 'app/Patient/patientId/VisitSummary/visitId';
 var VISIT_URL_PATTERN = 'app/Patient/patientId/Visit/visitType/visitId';
@@ -88874,7 +88874,6 @@ var ServiceManager = (function () {
             var dataType = obj.dataTypa;
             var timeout = obj.timeout;
             var contentType = obj.contentType;
-            console.log('obj: ' + JSON.stringify(obj));
 
             var promise = new Promise(function (resolve, reject) {
                 // console.log('Inside ServiceManager : function PromisedGet');
